5ecc807879be2966937b83a5cb486bed9c6e05ae
Brian Mericle <brian_mericle@choicehotels.com>
2017-09-01 16:31:06 -0500

    US0000 - updated pipeline job

--- src/main/jobs/pipelines/helloworldpipeline.groovy
+++ src/main/jobs/pipelines/helloworldpipeline.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/env groovy
 
+def scmUrl = 'https://github.com/bpmericle/jenkins-meetup-2017-09-05.git'
+def scmCredentialsId = 'github'
+def scmBranch = 'master'
+def scriptPath = 'src/main/jobs/pipelines/Jenkinsfile'
+
 pipelineJob('hello-world-pipeline') {
     displayName('Hello World Pipeline Job DSL Example')
     description('This Hello World pipeline job was created by using the Job DSL/Plugin.')
@@ -8,5 +13,20 @@ pipelineJob('hello-world-pipeline') {
 
     parameters {
         stringParam('NAME', '', 'Enter your name.')
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(scmUrl)
+                        credentials(scmCredentialsId)
+                    }
+                    branch(scmBranch)
+                }
+            }
+            scriptPath(scriptPath)
+        }
     }
 }
