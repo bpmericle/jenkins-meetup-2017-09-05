89f65ce999e9c06d3470c8d77d9edb781e5b0373
Brian Mericle <brian_mericle@choicehotels.com>
2017-09-01 13:58:49 -0500

    US0000 - first hello world job

---
+++ src/main/jobs/simple/simple.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+job('hello-world') {
+    displayName('Hello World Job DSL Example')
+    description('This Hello World job was created by using the Job DSL/Plugin.')
+
+    logRotator(-1,5,-1,-1)
+
+    parameters {
+        stringParam('NAME', '', 'Enter your name.')
+    }
+
+    steps {
+        shell("Hello ${NAME}.")
+    }
+}
